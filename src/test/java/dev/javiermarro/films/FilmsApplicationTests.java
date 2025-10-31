0afbb86a1c6769692804af2cfb6adf08ccd53e10
Javier <javier.martinezrom@gmail.com>
2025-10-31 15:32:57 +0000

    Initial commit, setting up MongoDB cluster database paired with Spring using IntelliJ IDEA

---
+++ src/test/java/dev/javiermarro/films/FilmsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.javiermarro.films;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
