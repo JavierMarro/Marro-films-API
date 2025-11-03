e196814daf50988bb83b5dc275c34cfe9a78fbc8
Javier <javier.martinezrom@gmail.com>
2025-11-03 15:10:31 +0000

    Review class created with needed Spring annotations

--- src/main/java/dev/javiermarro/films/Film.java
+++ src/main/java/dev/javiermarro/films/Film.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
@@ -24,4 +25,6 @@ public class Film {
     private String poster;
     private List<String> genres;
     private List<String> backdrops;
+    @DocumentReference // This makes the DB to store only the ids of the review while the reviews are in a separate collection (called manual reference relationships)
+    private List<Review> reviewList;
 }
