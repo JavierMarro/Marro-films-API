7472294bf8b0a5b676f717b62dcadc7e3d6996d1
Javier <javier.martinezrom@gmail.com>
2025-11-04 12:20:08 +0000

    Controller, model and repository implemented to request data from a specific film id

--- src/main/java/dev/javiermarro/films/FilmController.java
+++ src/main/java/dev/javiermarro/films/FilmController.java
@@ -1,13 +1,17 @@
 package dev.javiermarro.films;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/films") //any request made to the specified endpoint in brackets will be handled by this controller
@@ -20,6 +24,11 @@ public class FilmController {
     public ResponseEntity<List<Film>> getAllFilms(){
         // ResponseEntity<> helps return whichever data along with a full HTTP response (more in NOTES)
         return new ResponseEntity<List<Film>>(filmService.allFilms(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Film>> getFilmById(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Film>>(filmService.filmById(imdbId), HttpStatus.OK);
     }
 }
 
--- src/main/java/dev/javiermarro/films/FilmRepository.java
+++ src/main/java/dev/javiermarro/films/FilmRepository.java
@@ -4,9 +4,12 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 // this is the layer of the application that talks to the DB
 // it normally extends JpaRepository, CrudRepository, or MongoRepository (depending on DB)
-// defines the entity type and ID type (but can also define custom query methods)
+// defines the entity type, ID type and some built-in methods (but can also define custom query methods, for example findFilmByImdbId)
 @Repository
 public interface FilmRepository extends MongoRepository<Film, ObjectId> {
+    Optional<Film> findFilmByImdbId(String imdbId);
 }
