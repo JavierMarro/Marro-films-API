d8ab5fe01f1eccc44e839a6930659f67e93717de
Javier <javier.martinezrom@gmail.com>
2026-01-09 18:03:26 +0000

    Added CORS configuration folder and file

---
+++ src/main/java/dev/javiermarro/films/config/WebConfig.java
@@ -0,0 +1,24 @@
+package dev.javiermarro.films.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/v1/**")
+                        .allowedOrigins("http://localhost:5173") //TODO Change this allowedOrigin when front deployed to Netlify
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
+
