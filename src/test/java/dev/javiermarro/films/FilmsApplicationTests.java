9654960e3ba4c945b8cfd9ffa12ce5e6f4e13c26
Javier <javier.martinezrom@gmail.com>
2026-01-15 22:26:02 +0000

    enabling autoconfig for mongodb in CI tests

--- src/test/java/dev/javiermarro/films/FilmsApplicationTests.java
+++ src/test/java/dev/javiermarro/films/FilmsApplicationTests.java
@@ -1,18 +1,19 @@
 package dev.javiermarro.films;
 
-import dev.javiermarro.films.repositories.FilmRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+		MongoAutoConfiguration.class,
+		MongoDataAutoConfiguration.class
+})
 class FilmsApplicationTests {
 
-	@MockitoBean
-	private FilmRepository filmRepository;  // this mocks out the DB
-
 	@Test
-	void contextLoads() {}
+	void contextLoads() {
+	}
 }
-
