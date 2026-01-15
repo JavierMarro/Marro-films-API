b7498e2ae39e9a0541398747596e124bf3cc6181
Javier <javier.martinezrom@gmail.com>
2026-01-15 22:02:47 +0000

    added fix for tests MongoDB when building in CI

--- src/test/java/dev/javiermarro/films/FilmsApplicationTests.java
+++ src/test/java/dev/javiermarro/films/FilmsApplicationTests.java
@@ -1,12 +1,12 @@
 package dev.javiermarro.films;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
+@ActiveProfiles
 class FilmsApplicationTests {
 
 	@Test
