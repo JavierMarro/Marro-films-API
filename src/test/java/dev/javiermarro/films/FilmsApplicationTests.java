49e165f77e4ad61df11468a6d274625e9e9352c2
Javier <javier.martinezrom@gmail.com>
2026-01-15 22:16:29 +0000

    final attempt at not requiring MongoDB for CI build checks

--- src/test/java/dev/javiermarro/films/FilmsApplicationTests.java
+++ src/test/java/dev/javiermarro/films/FilmsApplicationTests.java
@@ -2,11 +2,14 @@ package dev.javiermarro.films;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
 @ActiveProfiles("test")
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*repository.*"))
 class FilmsApplicationTests {
 
 	@Test
