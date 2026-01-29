8f2f3351cdfd863f58150813a44bde8bb712bb73
Javier <javier.martinezrom@gmail.com>
2026-01-29 17:52:23 +0000

    Fixed SpringBootApplication issues via .env file + contextLoads from FilmsApplicationTests

---
+++ src/test/java/dev/javiermarro/films/FilmsApplicationTests.java
@@ -0,0 +1,14 @@
+package dev.javiermarro.films;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class FilmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
