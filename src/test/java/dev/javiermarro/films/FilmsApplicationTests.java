7c9f811207bf1a9a38f762f2062601b802d89d36
Javier <javier.martinezrom@gmail.com>
2026-01-30 19:57:11 +0000

    Updated pom.xml following Baeldung to get testcontainers to work

--- src/test/java/dev/javiermarro/films/FilmsApplicationTests.java
+++ src/test/java/dev/javiermarro/films/FilmsApplicationTests.java
@@ -1,11 +1,12 @@
 //package dev.javiermarro.films;
 //
+//import dev.javiermarro.films.support.AbstractBaseIntegrationTest;
 //import org.junit.jupiter.api.Test;
 //import org.springframework.boot.test.context.SpringBootTest;
 //
 //
 //@SpringBootTest
-//class FilmsApplicationTests {
+//class FilmsApplicationTests extends AbstractBaseIntegrationTest {
 //
 //	@Test
 //	void contextLoads() {
--- src/test/java/dev/javiermarro/films/services/ReviewServiceTests.java
+++ src/test/java/dev/javiermarro/films/services/ReviewServiceTests.java
@@ -2,3 +2,4 @@ package dev.javiermarro.films.services;
 
 public class ReviewServiceTests {
 }
+// TODO: Needs testcontainers for integration tests to the mongodb atlas
